36b18e59a871426047d7be6b0edff5b1a3d67141
Kumar Aditya <122738572+KumarAditya07@users.noreply.github.com>
2023-10-29 00:28:42 +0530

    Time: 8 ms (61.74%), Space: 10.7 MB (48.93%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,34 +11,53 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        vector<int> odd;
-        vector<int> even;
-        ListNode *curr = head;
-        int index = 1;
-        while(curr!=NULL){
-          if(index % 2 != 0) odd.push_back(curr->val);
-            else even.push_back(curr->val);
+      //naive solution  
+
+        // vector<int> odd;
+        // vector<int> even;
+        // ListNode *curr = head;
+        // int index = 1;
+        // while(curr!=NULL){
+        //   if(index % 2 != 0) odd.push_back(curr->val);
+        //     else even.push_back(curr->val);
           
-          curr=curr->next;
-          index++;
-        }
-          curr = head;
+        //   curr=curr->next;
+        //   index++;
+        // }
+        //   curr = head;
 
 
-        for(auto i : odd){
-          curr->val = i;
+        // for(auto i : odd){
+        //   curr->val = i;
 
-          curr=curr->next;
-        }
+        //   curr=curr->next;
+        // }
 
-        for(auto i : even){
-          curr->val = i;
+        // for(auto i : even){
+        //   curr->val = i;
 
-          curr=curr->next;
-        }
+        //   curr=curr->next;
+        // }
 
         
-         return head;
+        //  return head;
+ 
+
+      //using two pointer approch 
+       
+       if(head == NULL || head->next == NULL) return head;
+
+      ListNode *odd=head,*even=head->next,*evenHead=even;
+
+      while(odd->next!=NULL && even->next!= NULL){
+        odd->next=even->next;
+        odd=odd->next;
+        even->next=odd->next;
+        even=even->next;
+      }
+       odd->next=evenHead;
+
+      return head;
 
 
     }
