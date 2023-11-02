c23878c7137a7d188e68dfbefdd909d8233f7ac9
Kumar Aditya <122738572+KumarAditya07@users.noreply.github.com>
2023-11-02 21:20:36 +0530

    Time: 28 ms (25.57%), Space: 11.2 MB (77.42%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -25,6 +25,20 @@ public:
         
        return s;
 
+       //using only string
+
+        // int i=0;
+        // string ans;
+        // ans.push_back(s[i]);
+        // for(i=1;i<s.size();i++)
+        // {
+        //     if(s[i] == ans.back())
+        //         ans.pop_back();
+        //     else
+        //     ans.push_back(s[i]);
+        // }
+        // return ans;
+
         
     }
 };
