5ebf9255393030b8c3bfd5d297ad938f940cdd6f
Kumar Aditya <122738572+KumarAditya07@users.noreply.github.com>
2023-11-06 10:21:01 +0530

    Time: 16 ms (80.41%), Space: 16.6 MB (56.24%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -26,11 +26,49 @@ public:
     }
 };
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
+//optimised
+// /** initialize your data structure here. */
+//     stack<long> s;
+//     long mn;
+//     MinStack() {
+        
+//     }
+    
+//     void push(int x) {
+//         if(s.empty()){
+//             s.push(x);
+//             mn = x;
+//         }
+//         else if(x <= mn){
+//             // put 2*x - mn into stack
+//             s.push(2ll*x - mn);
+//             mn = x;
+//         }
+//         else{
+//             s.push(x);
+//         }
+//     }
+    
+//     void pop() {
+//         if(s.empty()) return;
+//         // stack top can never be smaller than mn, if it is then it indicates a flag which means before popping, you have to update mn
+//         if(s.top() < mn){
+//             // mn = 2*mn - s.top()
+//             mn = 2ll*mn - s.top();
+//         }
+//         s.pop();
+//     }
+    
+//     // critical part
+//     int top() {
+//         if(s.empty()) return -1;
+//         if(s.top() < mn){
+//             return mn;
+//         }
+//         return s.top();
+//     }
+    
+//     int getMin() {
+//         if(s.empty()) return -1;
+//         return mn;
+//     }
