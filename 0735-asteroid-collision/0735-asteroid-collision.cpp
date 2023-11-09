422e26af3a6670b2fd8851c3b2e2b835adcc7d74
Kumar Aditya <122738572+KumarAditya07@users.noreply.github.com>
2023-11-09 09:11:38 +0530

    Time: 11 ms (54.86%), Space: 18.1 MB (65.9%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,26 +1,46 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& ast) {
-        stack<int>st;
+        stack<int>s;
         vector<int>ans;
-        for(int i =0;i<ast.size();i++){
-            if(!st.empty() && ast[i]<0  && st.top()>0 && ast[i]<0){
-              while(!st.empty() && abs(st.top())<abs(ast[i]) && st.top()>0 && ast[i]<0){
-                  st.pop();
-          } 
+        // for(int i =0;i<ast.size();i++){
+        //     if(!s.empty() && ast[i]<0  && s.top()>0 && ast[i]<0){
+        //       while(!s.empty() && abs(s.top())<abs(ast[i]) && s.top()>0 && ast[i]<0){
+        //           s.pop();
+        //   } 
+        //     }
+        //     if(!s.empty() && ast[i]<0 && abs(s.top())==abs(ast[i]) && s.top()>0 && ast[i]<0 ){
+        //       s.pop();
+        //       continue;
+        //     }
+        //      else if(!s.empty() && ast[i]<0 && abs(s.top())>abs(ast[i])  && s.top()>0 && ast[i]<0 ){
+        //       continue;
+        //     }
+        //     s.push(ast[i]);
+        // }
+
+                                                     //or
+           for(int i = 0; i < ast.size(); i++) {
+            if(ast[i] > 0 || s.empty()) {
+                s.push(ast[i]);
             }
-            if(!st.empty() && ast[i]<0 && abs(st.top())==abs(ast[i]) && st.top()>0 && ast[i]<0 ){
-              st.pop();
-              continue;
+            else {
+                while(!s.empty() and s.top() > 0 and s.top() < abs(ast[i])) {
+                    s.pop();
+                }
+                if(!s.empty() and s.top() == abs(ast[i])) {
+                    s.pop();
+                }
+                else {
+                    if(s.empty() || s.top() < 0) {
+                        s.push(ast[i]);
+                    }
+                }
             }
-             else if(!st.empty() && ast[i]<0 && abs(st.top())>abs(ast[i])  && st.top()>0 && ast[i]<0 ){
-              continue;
-            }
-            st.push(ast[i]);
         }
-        while(!st.empty()){
-          ans.push_back(st.top());
-          st.pop();
+        while(!s.empty()){
+          ans.push_back(s.top());
+          s.pop();
         }
         reverse(ans.begin(),ans.end());
         return ans;
